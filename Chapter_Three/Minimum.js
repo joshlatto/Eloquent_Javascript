5288d7c0f2a94236d856a08a9b28c056b83ff4ae
Josh Latto <josh.latto@gmail.com>
2017-01-18 16:56:50 -0800

    Solution to chapter three, excercise one.

---
+++ Chapter_Three/Minimum.js
@@ -0,0 +1,27 @@
+/*
+PROBLEM STATEMENT
+
+Minimum
+
+The previous chapter introduced the standard function Math.min that returns its smallest argument. We can do that ourselves now. Write a function "min" that takes two arguments and returns their minimum. 
+
+Solution by: Josh Latto
+Date: 12/23/2016
+*/
+
+var my_min = function(a, b) {
+	
+	// Check if a is the minimum
+	if (a < b) { 
+		console.log(a);
+	} else { 
+		console.log(b);
+	};
+
+}
+
+my_min(4, 10);
+
+my_min(4, 4);
+
+my_min(7, 3);
